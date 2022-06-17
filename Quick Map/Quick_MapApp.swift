c95fa5a2f8d0906f69773fab3f4e553e8eb08fc5
Chat Dep <42887325+chatsopond@users.noreply.github.com>
2022-06-17 16:29:11 +0700

    Initial Commit

---
+++ Quick Map/Quick_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_MapApp.swift
+//  Quick Map
+//
+//  Created by Chatsopon Deepateep on 17/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Quick_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
